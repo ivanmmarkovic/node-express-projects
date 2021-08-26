8fded795df95c501763832331e826b319e2a2b34
Ivan Markovic <ivanmdmarkovic@gmail.com>
2021-08-27 00:18:31 +0200

    Async routes and 404 page

---
+++ 03-static-site-assets-async-routes-handlers-404-page/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', async (req, res) => {
+    res.render('index', {'title': 'Index'});
+});
+
+app.get('/about', async (req, res) => {
+    res.render('about', {'title': 'About'});
+});
+
+app.get('/contact', async (req, res) => {
+    res.render('contact', {'title': 'Contact'});
+});
+
+app.use('*', async (req, res) => res.render('404', {'title': '404'}));
+
+app.listen(5000, () => console.log('Listening on port 5000'));
