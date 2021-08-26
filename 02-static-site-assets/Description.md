f8aad1555ed7a764f7b8c835c685a482ae0a1f74
Ivan Markovic <ivanmdmarkovic@gmail.com>
2021-08-27 00:11:05 +0200

    Static site with assets

---
+++ 02-static-site-assets/Description.md
@@ -0,0 +1,13 @@
+
+## Project description
+
+- Three static pages
+- Header with links
+- ejs templating
+- assets(css, images)
+
+## Start project
+
+- Run *npm install*
+- Run *npm run start-dev*
+- Visit [http://localhost:5000](http://localhost:5000) with browser
---
+++ 02-static-site-assets/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+    res.render('index', {'title': 'Index'});
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {'title': 'About'});
+});
+
+app.get('/contact', (req, res) => {
+    res.render('contact', {'title': 'Contact'});
+});
+
+app.listen(5000, () => console.log('Listening on port 5000'));
