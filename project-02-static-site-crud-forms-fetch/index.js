247f8c5efb4d007bb2823d5d3725e5b75310b1e1
Ivan Markovic <ivanmdmarkovic@gmail.com>
2021-09-19 20:04:27 +0200

    Removed controllers/ArticleController

--- project-02-static-site-crud-forms-fetch/index.js
+++ project-02-static-site-crud-forms-fetch/index.js
@@ -10,7 +10,12 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', async (req, res, next) => {
-	res.render('index', {title: 'Index'});
+    try {
+        let articles = await ArticleModel.find();
+        res.render('index', {title: 'Index', articles});
+    } catch (error) {
+        res.render('error');
+    }
 });
 
 
