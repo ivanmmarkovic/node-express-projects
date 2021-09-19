7f02a11269eaf0a3284ecd14bb75f617fe2103ee
Ivan Markovic <ivanmdmarkovic@gmail.com>
2021-09-19 19:44:08 +0200

    project setup

---
+++ project-02-static-site-crud-forms-fetch/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', async (req, res, next) => {
+	
+});
