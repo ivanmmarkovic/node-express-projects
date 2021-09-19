d2973b91012550937a2caf6fbf6419862532a0a5
Ivan Markovic <ivanmdmarkovic@gmail.com>
2021-09-19 19:49:18 +0200

    index page

--- project-02-static-site-crud-forms-fetch/index.js
+++ project-02-static-site-crud-forms-fetch/index.js
@@ -8,5 +8,8 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', async (req, res, next) => {
-	
+	res.render('index', {title: 'Index'});
 });
+
+
+app.listen(5000, () => console.log('Listen on port 5000'));
