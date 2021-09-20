0ed7dfac16c1383b0c37a9acb5d1ff93b96d6622
Ivan Markovic <ivanmdmarkovic@gmail.com>
2021-09-21 00:21:32 +0200

    Project 03 starter files

---
+++ project-03-static-site-login-logout/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://admin:password@localhost:27017/articles?authSource=admin', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+});
+
+const ArticleModel = require('./models/Article');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+
+app.get('/', async (req, res, next) => {
+    res.render('index', {title: 'Index'});
+});
+
+app.use('*', async (req, res) => res.render('404', {'title': '404'}));
+
+app.listen(5000, () => console.log('Listen on port 5000'));
