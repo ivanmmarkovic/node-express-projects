8433dfee97850e51810495b25dc4b8fe3f419af5
Ivan Markovic <ivanmdmarkovic@gmail.com>
2021-09-22 23:58:16 +0200

    Login/create

--- project-03-static-site-login-logout/index.js
+++ project-03-static-site-login-logout/index.js
@@ -9,6 +9,9 @@ mongoose.connect('mongodb://admin:password@localhost:27017/articles?authSource=a
     useUnifiedTopology: true 
 });
 
+const UserModel = require('./models/User');
+
+const bcyrpt = require('bcrypt');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,8 +25,39 @@ app.get('/', async (req, res, next) => {
 });
 
 app.get('/create', async (req, res, next) => {
-    res.render('index', {title: 'Create'});
+    res.render('create', {title: 'Create'});
 });
+
+app.post('/create', async (req, res, next) => {
+    let body = {...req.body};
+    body.password = await bcyrpt.hash(body.password, 5);
+    let user = await UserModel.create({...body});
+    res.render('index', {title: 'Index'});
+});
+
+app.get('/login', async (req, res, next) => {
+    res.render('login', {title: 'Login'});
+});
+
+app.post('/login', async (req, res, next) => {
+    let {username, email, password} = {...req.body};
+    let user = await UserModel.findOne({username});
+    let matches = await bcyrpt.compare(password, user.password);
+    if(matches)
+        res.render('index', {title: 'Index'});
+    else
+        res.render('login', {title: 'Login'});
+});
+
+
+app.get('/public', async (req, res, next) => {
+    res.render('public', {title: 'Public'});
+});
+
+app.get('/private', async (req, res, next) => {
+    res.render('private', {title: 'Private'});
+});
+
 
 app.use('*', async (req, res) => res.render('404', {'title': '404'}));
 
