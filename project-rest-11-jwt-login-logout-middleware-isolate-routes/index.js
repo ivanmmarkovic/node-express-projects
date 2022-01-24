4b94e4559fa9d8132ddf8a8736ba5b957bb8eb7d
Ivan Markovic <ivanmdmarkovic@gmail.com>
2022-01-24 19:47:57 +0100

    AuthMiddleware added

--- project-rest-11-jwt-login-logout-middleware-isolate-routes/index.js
+++ project-rest-11-jwt-login-logout-middleware-isolate-routes/index.js
@@ -13,34 +13,10 @@ mongoose.connect('mongodb://admin:password@localhost:27017/articles?authSource=a
 global.jwtKey = "secret";
 global.jwtExpires = 24 * 60 * 60 * 1000;
 
-const UserModel = require('./models/User');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-let authMiddleware = (req, res, next) => {
-    let bearer = req.header("Authorization");
-    if(!bearer){
-        res.status(401);
-        res.send({message: "Unauthorized"});
-    }
-    try {
-        let [, token] = bearer.split(" ");
-        payload = jwt.verify(token, global.jwtKey);
-        // add payload to req
-        req.payload = payload;
-        next();
-    } catch (error) {
-        if(error instanceof jwt.JsonWebTokenError){
-            res.status(401);
-            return res.send({message: "Unauthorized"});
-        }
-        else{
-            res.status(400);
-            return res.send({message: "Bad Request"});
-        }
-    }
-};
 
 
 app.get("/public", async (req, res, next) => {
