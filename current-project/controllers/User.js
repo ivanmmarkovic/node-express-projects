0cf1c471a8858c4884f49e44f9d8a76aff60e3e9
Ivan Markovic <ivanmdmarkovic@gmail.com>
2022-01-31 23:05:59 +0100

    Add bcrypt for password hashing

--- current-project/controllers/User.js
+++ current-project/controllers/User.js
@@ -1,9 +1,14 @@
+
+const bcrypt = require('bcrypt');
+
 const UserModel = require('../models/User');
+
 
 
 const create = async (req, res, next) => {
     try {
         let {username, email, password} = req.body;
+        password = await bcrypt.hash(password, 10);
         let user = await UserModel.create({username, email, password, createdAt: new Date(), updatedAt: new Date()});
         res.status(201).json(user);
     } catch (error) {
@@ -35,6 +40,9 @@ const findById = async (req, res, next) => {
 const updateById = async (req, res, next) => {
     try {
         let {id} = req.params;
+        if(req.body.password){
+            req.body.password = await bcrypt.hash(req.body.password, 10);
+        }
         let user = await UserModel.findByIdAndUpdate(id, {...req.body, updatedAt: new Date()}, {new: true});
         res.status(200).json(user);
     } catch (error) {
