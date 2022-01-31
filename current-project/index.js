c2cf7baf528939b25b372cfd0ffecebd972c051e
Ivan Markovic <ivanmdmarkovic@gmail.com>
2022-01-31 19:55:39 +0100

    Base project setup

---
+++ current-project/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(5000, () => console.log('App listens on port 5000'));
