8d0069c0a98aeaef7a830e8f2c8449ea2f2f579f
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-05-26 01:35:00 +0200

    Add catch block for handling errors

--- rest-project-01-crud/app.js
+++ rest-project-01-crud/app.js
@@ -14,46 +14,67 @@ app.use(express.json());
 
 
 app.post('/articles', async (req, res, next) => {
-    let {title, body} = req.body;
-    let article = await ArticleModel.create({title, body});
-    res.status(201);
-    res.json({
-        article
-    });
+    try {
+        let {title, body} = req.body;
+        let article = await ArticleModel.create({title, body});
+        res.status(201);
+        res.json({
+            article
+        });
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 
 app.get("/articles", async (req, res, next) => {
-    let articles = await ArticleModel.find();
-    res.json({
-        articles
-    })
+    try {
+        let articles = await ArticleModel.find();
+        res.json({
+            articles
+        });
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 
 app.get("/articles/:id", async (req, res, next) => {
-    let {id} = req.params;
-    let article = await ArticleModel.findById(id);
-    res.json({
-        article
-    })
+    try {
+        let {id} = req.params;
+        let article = await ArticleModel.findById(id);
+        res.json({
+            article
+        });
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 
 app.patch("/articles/:id", async (req, res, next) => {
-    let {id} = req.params;
-    let article = await ArticleModel.findByIdAndUpdate(id, {...req.body}, {new: true});
-    res.json({
-        article
-    })
+    try {
+        let {id} = req.params;
+        let article = await ArticleModel.findByIdAndUpdate(id, {...req.body}, {new: true});
+        res.json({
+            article
+        });
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 
 app.delete('/articles/:id', async (req, res, next) => {
-    let {id} = req.params;
-    //await ArticleModel.deleteOne({"_id": id});
-    await ArticleModel.findByIdAndDelete(id); // same as code above
-    res.status(204).json(null);
+    try {
+        let {id} = req.params;
+        //await ArticleModel.deleteOne({"_id": id});
+        await ArticleModel.findByIdAndDelete(id); // same as code above
+        res.status(204).json(null);
+        
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 app.listen(5000);
