82fbb8140277d3be020c4f96200e0e474d74849c
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-05-27 01:46:45 +0200

    docker-compose.yaml

--- exercise/index.js
+++ exercise/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://admin:password@localhost:27017/articles?authSource=admin', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+});
 
 
 
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.listen(5000);
