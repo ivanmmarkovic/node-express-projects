cfeac24f79bf04e1b5fb22a5fb6bb88a078deead
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-05-30 23:58:21 +0200

    Add User model

---
+++ exercise/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    username: {
+        type:String,
+        unique: true
+    },
+    email: {
+        type:String,
+        unique: true
+    },
+    password: {
+        type:String,
+        required: true
+    },
+    createdAt: {
+        type:Date
+    }
+});
+
+
+const UserModel = mongoose.model('UserModel', UserSchema);
+module.exports = UserModel;
