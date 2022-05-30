4ea953710785fd37ad250729662a0fd9fd4c4c63
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-05-31 00:32:42 +0200

    Handling errors

--- exercise/app.js
+++ exercise/app.js
@@ -13,33 +13,53 @@ app.use(express.json());
 
 
 app.post('/users', async (req, res, next) => {
-    let user = await UserModel.create({...req.body, createdAt: new Date(), updatedAt: new Date()});
-    res.status(201).json(user);
+    try {
+        let user = await UserModel.create({...req.body, createdAt: new Date(), updatedAt: new Date()});
+        res.status(201).json(user);
+    } catch (error) {
+        next(error);
+    }
 });
 
 
 app.get('/users', async (req, res, next) => {
-    let users = await UserModel.find();
-    res.status(200).json(users);
+    try {
+        let users = await UserModel.find();
+        res.status(200).json(users);
+    } catch (error) {
+        next(error);
+    }
 });
 
 app.get('/users/:id', async (req, res, next) => {
-    let {id} = req.params;
-    let user = await UserModel.findById(id);
-    res.status(200).json(user);
+    try {
+        let {id} = req.params;
+        let user = await UserModel.findById(id);
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
 });
 
 
 app.put('/users/:id', async (req, res, next) => {
-    let {id} = req.params;
-    let user = await UserModel.findByIdAndUpdate(id, {...req.body, updatedAt: new Date()}, {new: true});
-    res.status(200).json(user);
+    try {
+        let {id} = req.params;
+        let user = await UserModel.findByIdAndUpdate(id, {...req.body, updatedAt: new Date()}, {new: true});
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
 });
 
 app.delete('/users/:id', async (req, res, next) => {
-    let {id} = req.params;
-    let user = await UserModel.findByIdAndDelete(id);
-    res.status(204).json(null);
+    try {
+        let {id} = req.params;
+        let user = await UserModel.findByIdAndDelete(id);
+        res.status(204).json(null);
+    } catch (error) {
+        next(error);
+    }
 });
 
 
