677ad2dae561d9fc5bd91cb2c95ae76276f888e4
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-06-09 00:57:24 +0200

    Project setup

--- full-project-01-social-media-rest-api/app.js
+++ full-project-01-social-media-rest-api/app.js
@@ -7,11 +7,22 @@ const morgan = require('morgan');
 
 dotenv.config();
 
-
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true 
 }, () => console.log('Connected to mongo'));
+
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+
+app.get('/', async (req, res, next) => {
+    return res.status(200).json({message: 'Hello'})
+});
+
 
 
 app.listen(5000, () => console.log('Listening on port 5000'));
