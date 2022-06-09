f64386aa86cabb0a8a06f99908c20ddc0b9270dd
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-06-10 00:50:23 +0200

    update finished

--- full-project-01-social-media-rest-api/routes/User.js
+++ full-project-01-social-media-rest-api/routes/User.js
@@ -1,6 +1,30 @@
 
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const UserModel = require('../models/User');
 
-
+router.put('/:id', async (req, res, next) => {
+    if(req.body.userId == req.params.id || req.user.isAdmin){
+        if(req.body.password){
+            try {
+                const salt = await bcrypt.genSalt(10);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
+            } catch (error) {
+                return res.status(500).json(err);
+            }
+            try {
+                const user = await UserModel.findByIdAndUpdate(req.params.id, {
+                    $set: req.body
+                });
+                return res.status(200).json('Account has been updated');
+            } catch (error) {
+                return res.status(500).json(err);
+            }
+        }
+    }
+    else{
+        return res.status(403).json('You can update only your account');
+    }
+});
 
 module.exports = router;
