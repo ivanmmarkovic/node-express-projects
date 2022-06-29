7724743a595e803020653fc2fb8996280bcc271e
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-06-30 01:36:20 +0200

    dotenv

--- auth-01-google-auth/app.js
+++ auth-01-google-auth/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
 const app = express();
 
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res, next) => {
     res.json('hello');
