5afe4c4b7f82b2c6c02894c9186a256522cc351a
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-07-06 23:44:30 +0200

    WebSocket server setup

--- websockets/working-project/server/index.js
+++ websockets/working-project/server/index.js
@@ -1,10 +1,20 @@
 const WebSocket = require('ws');
 
+/*
 const wss = new WebSocket.Server({
     port: 9876
 }, function(){
     // this function executes when websocket is ready
+   
+});
+*/
 
+const wss = new WebSocket.Server({
+    port: 9876
 });
 
 
+// when client connects to server, this message will be sent
+wss.on('connection', function(ws){
+    ws.send('Hello from the server');
+});
