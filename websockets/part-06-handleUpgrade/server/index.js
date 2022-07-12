e56969cd77b11e91345d9705f15910df140a4da0
Ivan Markovic <ivanmdmarkovic@gmail.com>
2022-07-13 01:28:02 +0200

    minor fix

--- websockets/part-06-handleUpgrade/server/index.js
+++ websockets/part-06-handleUpgrade/server/index.js
@@ -13,7 +13,7 @@ app.get('/', async (req, res, next) => {
 });
 
 
-// app.listen retuns a server
+// app.listen returns a server
 const server = app.listen(9876);
 
 const wss = new WebSocket.Server({
