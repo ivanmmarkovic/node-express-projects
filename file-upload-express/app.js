b70d51ff0263e6165091af884914b390ba00e18c
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-07-16 19:00:55 +0200

    Upload route and fileUpload middleware

--- file-upload-express/app.js
+++ file-upload-express/app.js
@@ -12,5 +12,17 @@ app.get('/', async (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.post('/upload', fileUpload({
+    createParentPath: true
+}), (req, res) => {
+    const files = req.files;
+    console.log(files);
+
+    return res.json({
+        status: 'logged',
+        message: 'logged'
+    })
+});
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
