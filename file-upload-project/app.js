b1c754f4c81ddc93cc360fa98720afa323a27155
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-07-17 18:56:46 +0200

    Handle upload on frontend and backend

--- file-upload-project/app.js
+++ file-upload-project/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fileUpload = require('express-fileupload');
 
 const PORT = process.env.PORT || 5000;
 
@@ -7,10 +8,14 @@ const ejs = require('ejs');
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
-
+app.use(fileUpload());
 
 app.get('/', async (req, res, next) => {
     res.render('index', {title: 'Home'});
+});
+
+app.post('/upload', async (req, res, next) => {
+    console.log(req.files);
 });
 
 
--- file-upload-project/public/js/index.js
+++ file-upload-project/public/js/index.js
@@ -1,3 +1,38 @@
 
 
-console.log('----------------');
+const myForm = document.getElementById('myForm');
+const myFile = document.getElementById('myFile');
+const btn = document.getElementById('btn');
+
+
+console.log(myForm, myFile, btn);
+
+
+btn.addEventListener('click', handleUpload);
+
+async function handleUpload(e){
+    e.preventDefault();
+    let files = myFile.files;
+
+    let formData = new FormData();
+
+    for(let file of files){
+        console.log(file.name, file);
+        console.log('------------------------');
+        formData.append(file.name, file);
+    }
+
+
+    console.log(formData);
+
+    let response = await fetch('http://localhost:5000/upload', {
+        method: 'POST',
+        body: formData
+    });
+
+    let json = await response.json();
+
+    console.log(json);
+
+};
+
