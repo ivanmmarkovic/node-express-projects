cab005d4d1ae70bf576c7308bc52b36266ab6db9
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-07-17 19:19:32 +0200

    Finished project

--- file-upload-project/app.js
+++ file-upload-project/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const fileUpload = require('express-fileupload');
@@ -8,14 +9,31 @@ const ejs = require('ejs');
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
-app.use(fileUpload());
+app.use(fileUpload({createParentPath: true}));
 
 app.get('/', async (req, res, next) => {
     res.render('index', {title: 'Home'});
 });
 
 app.post('/upload', async (req, res, next) => {
-    console.log(req.files);
+    // req.files -> Object
+    console.log(Object.keys(req.files)); // return array of file(s) name as key
+    Object.keys(req.files).forEach(key => {
+        const filepath = path.join(__dirname, '/files', req.files[key].name);
+        req.files[key].mv(filepath, (err) => {
+            if(err){
+                return res.status(500).json({
+                    status: 'error',
+                    message: err
+                })
+            }
+        });
+    });
+
+    return res.json({
+        status: 'success',
+        message: Object.keys(req.files).toString()
+    })
 });
 
 
--- file-upload-project/public/js/index.js
+++ file-upload-project/public/js/index.js
@@ -1,38 +1,38 @@
 
-
 const myForm = document.getElementById('myForm');
 const myFile = document.getElementById('myFile');
 const btn = document.getElementById('btn');
 
-
-console.log(myForm, myFile, btn);
-
+const p = document.querySelector('p');
+const span = document.querySelector('span');
 
 btn.addEventListener('click', handleUpload);
 
 async function handleUpload(e){
     e.preventDefault();
     let files = myFile.files;
-
     let formData = new FormData();
 
     for(let file of files){
-        console.log(file.name, file);
-        console.log('------------------------');
         formData.append(file.name, file);
     }
 
+    try {
+        let response = await fetch('http://localhost:5000/upload', {
+            method: 'POST',
+            body: formData
+        });
+    
+        let json = await response.json();
+        let {status, message} = json;
+        p.textContent = status;
+        span.textContent = message;
+    } catch (error) {
+        let {status, message} = error;
+        p.textContent = status;
+        span.textContent = message;
+    }
 
-    console.log(formData);
-
-    let response = await fetch('http://localhost:5000/upload', {
-        method: 'POST',
-        body: formData
-    });
-
-    let json = await response.json();
-
-    console.log(json);
 
 };
 
