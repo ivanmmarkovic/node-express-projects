cdb25cdeb091214130d16cf14a079e4c51242ffc
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-07-24 16:59:00 +0200

    ecommerce-app/ecommerce-rest-mongo-jwt/routes/Product.js

--- ecommerce-app/ecommerce-rest-mongo-jwt/routes/Product.js
+++ ecommerce-app/ecommerce-rest-mongo-jwt/routes/Product.js
@@ -1,5 +1,84 @@
+const ProductModel = require("../models/Product");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
+//CREATE
+
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+  const newProduct = new ProductModel(req.body);
+
+  try {
+    const savedProduct = await newProduct.save();
+    return res.status(200).json(savedProduct);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//UPDATE
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await ProductModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    return res.status(200).json(updatedProduct);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//DELETE
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await ProductModel.findByIdAndDelete(req.params.id);
+    return res.status(200).json("Product has been deleted...");
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//GET PRODUCT
+router.get("/find/:id", async (req, res) => {
+  try {
+    const product = await ProductModel.findById(req.params.id);
+    return res.status(200).json(product);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//GET ALL PRODUCTS
+router.get("/", async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try {
+    let products;
+
+    if (qNew) {
+      products = await ProductModel.find().sort({ createdAt: -1 }).limit(1);
+    } else if (qCategory) {
+      products = await ProductModel.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await ProductModel.find();
+    }
+
+    return res.status(200).json(products);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
 module.exports = router;
