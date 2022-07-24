67e89d157dd39dca75eac5bc45fb361ed95a399d
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-07-24 17:01:20 +0200

    ecommerce-app/ecommerce-rest-mongo-jwt/index.js

--- ecommerce-app/ecommerce-rest-mongo-jwt/index.js
+++ ecommerce-app/ecommerce-rest-mongo-jwt/index.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRouter = require('./routes/User');
 const authRouter = require('./routes/Auth');
+const productRouter = require('./routes/Product');
+const cartRouter = require('./routes/Cart');
+const orderRouter = require('./routes/Order');
 
 dotenv.config();
 
@@ -21,5 +24,8 @@ mongoose
 app.use(express.json());
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
+app.use('/api/product', productRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
 
 app.listen(PORT, () => console.log(`Server listen on port ${PORT}`));
