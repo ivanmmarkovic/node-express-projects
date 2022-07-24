4e9c6c7aca488d317449221480288a5a0b533b89
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-07-24 16:59:58 +0200

    ecommerce-app/ecommerce-rest-mongo-jwt/routes/Cart.js

--- ecommerce-app/ecommerce-rest-mongo-jwt/routes/Cart.js
+++ ecommerce-app/ecommerce-rest-mongo-jwt/routes/Cart.js
@@ -1,5 +1,70 @@
+const CartModel = require("../models/Cart");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
+//CREATE
+
+router.post("/", verifyToken, async (req, res) => {
+  const newCart = new CartModel(req.body);
+
+  try {
+    const savedCart = await newCart.save();
+    return res.status(200).json(savedCart);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const updatedCart = await CartModel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    return res.status(200).json(updatedCart);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//DELETE
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await CartModel.findByIdAndDelete(req.params.id);
+    return res.status(200).json("Cart has been deleted...");
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+//GET USER CART
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const cart = await CartModel.findOne({ userId: req.params.userId });
+    return res.status(200).json(cart);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// //GET ALL
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const carts = await CartModel.find();
+    return res.status(200).json(carts);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
 module.exports = router;
