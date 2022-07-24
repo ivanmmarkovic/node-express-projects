4062935eb059ac24472d47a75968272316d9b85e
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-07-24 17:00:19 +0200

    ecommerce-app/ecommerce-rest-mongo-jwt/routes/Auth.js

--- ecommerce-app/ecommerce-rest-mongo-jwt/routes/Auth.js
+++ ecommerce-app/ecommerce-rest-mongo-jwt/routes/Auth.js
@@ -2,6 +2,7 @@
 const router = require('express').Router();
 const UserModel = require('../models/User');
 const CryptoJS = require("crypto-js");
+const jwt = require('jsonwebtoken');
 
 router.post('/register', async (req, res) => {
 
@@ -20,6 +21,54 @@ router.post('/register', async (req, res) => {
         const savedUser = await newUser.save();
         return res.status(201).json(savedUser);
     } catch (error) {
+        return res.status(500).json(err);
+    }
+
+});
+
+
+router.post('/login', async (req, res) => {
+    try{
+        const user = await UserModel.findOne(
+            {
+                userName: req.body.user_name
+            }
+        );
+
+        // !user && res.status(401).json("Wrong User Name");
+
+        if(!user ){
+            return res.status(401).json("Wrong User Name");
+        }
+
+        const hashedPassword = CryptoJS.AES.decrypt(
+            user.password,
+            process.env.PASS_SEC
+        );
+
+
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+        const inputPassword = req.body.password;
+        
+        // originalPassword != inputPassword && res.status(401).json("Wrong Password");
+        
+        if(originalPassword != inputPassword){
+            return res.status(401).json("Wrong Password");
+        }
+
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin,
+        },
+        process.env.JWT_SEC,
+            {expiresIn:"3d"}
+        );
+  
+        const { password, ...others } = user._doc;  
+        return res.status(200).json({...others, accessToken});
+
+    }catch(err){
         return res.status(500).json(err);
     }
 
