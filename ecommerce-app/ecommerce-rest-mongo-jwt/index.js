c50c5ddd56c79e9944ef29a04f28147130ebcc8b
ivanmmarkovic <ivanmdmarkovic@gmail.com>
2022-07-24 13:57:38 +0200

    Setup MongoDB and .env

--- ecommerce-app/ecommerce-rest-mongo-jwt/index.js
+++ ecommerce-app/ecommerce-rest-mongo-jwt/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true 
+    })
+    .then(() => console.log('DB connection successfull'))
+    .catch(err => console.log(err));
+
+
 
 app.listen(PORT, () => console.log(`Server listen on port ${PORT}`));
